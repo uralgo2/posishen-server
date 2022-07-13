bbb872125ec6cf7e4b37f5254b74905460b27db4
uralgo2 <63842179+uralgo2@users.noreply.github.com>
2022-07-13 23:31:33 +0500

    wrote backend and sql events, added readme (not completed)

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dbName: '',
+    dbUsername: '',
+    dbPort: 3310,
+    dbPassword: '',
+    dbHost: '',
+
+    smtpHost: '',
+    smtpPort: 0,
+    smtpSecure: false,
+    smtpEmail: '',
+    smtpPassword: ''
+}
