509a43597fc253b5aef8b3679e44ffa5ac06cc10
uralgo2 <63842179+uralgo2@users.noreply.github.com>
2022-07-16 08:26:00 +0500

    wrote backend and sql events, added readme (not completed)

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,39 @@ const fs = require("fs")
 const config = require("../config");
 const logger = require('log4js').getLogger('pozishen')
 
+const mysql = require("mysql2/promise")
+
+let connection = null
+
+async function handleDisconnect() {
+    connection = await mysql.createConnection({
+        host: config.dbHost,
+        user: config.dbUsername,
+        database: config.dbName,
+        password: config.dbPassword,
+        port: config.dbPort
+    })
+    connection.connect((e) => {
+        if(e){
+            logger.error("Не удалось подключиться к серверу MySql: %s", e)
+            setTimeout(handleDisconnect, 2000)
+        }
+    })
+    connection.on('error', (e) => {
+        logger.error("Ошибка базы данных: %s", e)
+        if(e.code === 'PROTOCOL_CONNECTION_LOST')
+            handleDisconnect()
+        else
+            throw e
+    })
+}
+
+handleDisconnect().then(()=>logger.info("Успешное подключение к серверу MySql"))
+
+setInterval(()=>{
+    connection.query('SELECT 1')
+}, 10000)
+
 let transporter
 transporter = nodemailer.createTransport({
     host: config.smtpHost,
