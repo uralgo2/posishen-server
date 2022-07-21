5f789bd655e57c177ebac278c38381456c00915b
uralgo2 <63842179+uralgo2@users.noreply.github.com>
2022-07-21 18:29:10 +0500

    wrote backend and sql events, added readme (not completed)

--- config.js
+++ config.js
@@ -1,13 +1,28 @@
-module.exports = {
-    dbName: '',
-    dbUsername: '',
-    dbPort: 3310,
-    dbPassword: '',
-    dbHost: '',
+const fs = require('fs')
+let key_path = '' // путь для приватного dkim ключа
 
-    smtpHost: '',
-    smtpPort: 0,
-    smtpSecure: false,
-    smtpEmail: '',
-    smtpPassword: ''
+let key = fs.readFileSync(key_path).toString('utf8')
+
+module.exports = {
+    dbName: 'pozishen', // имя базы данных
+    dbUsername: 'root', // имя пользователя
+    dbPort: 3306, // порт сервера базы данных
+    dbPassword: '', // пароль пользователя
+    dbHost: '', // хост сервера базы данных
+
+    smtpHost: '', // хост почтового сервера
+    smtpPort: 0, // порт почтового сервера
+    smtpSecure: false, // защищенное соединение? да - true, нет - false
+    smtpEmail: '', // электронная почта
+    smtpPassword: '', // пароль почты
+    clientPath: '', // путь к клиенту
+    /*dkim: {
+        domainName: "pozishen.ru", // имя домена сервера почты
+        keySelector: "default", // селектор
+        privateKey: key // ключ
+    },*/
+    // in seconds
+    taskWaitTimeout: 5 * 60, // время ожидания завершения задачи
+    programOfflineTimeout: 6 * 60, // время ожидания следущего запроса от клиента
+    keyOffset: 0x2eb190 // смещение для записи ключа
 }
